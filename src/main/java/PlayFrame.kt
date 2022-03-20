a41437714431585872b107b2b040f44eb4ab19df
Anrew <35604383+Anreway@users.noreply.github.com>
2022-03-20 10:48:33 +0700

    WOW

--- src/main/java/PlayFrame.kt
+++ src/main/java/PlayFrame.kt
@@ -1,9 +1,18 @@
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.yield
 import java.awt.Graphics
 import java.awt.event.MouseEvent
 import java.awt.event.MouseMotionListener
 import javax.swing.JFrame
+import kotlin.math.abs
 
 class PlayFrame : JFrame() {
+    var newPosition = 0.0 to 0.0
+        set(value) {
+            println(value)
+            field = value
+        }
     var position = 0.0 to 0.0
         set(value) {
             println(value)
@@ -15,16 +24,26 @@ class PlayFrame : JFrame() {
         addMouseMotionListener(object : MouseMotionListener {
             override fun mouseDragged(e: MouseEvent?) {}
             override fun mouseMoved(e: MouseEvent) {
-                val sx = (e.x - position.first)
-                val sy = (e.y - position.second)
-                val dx = sx / speed * 0.03
-                val dy = sy / speed * 0.03
-                position = position.first + dx to position.second + dy
-                updateCoord(
-                    position
-                )
+                newPosition = e.x.toDouble() to e.y.toDouble()
             }
         })
+        GlobalScope.launch {
+            while (true){
+                yield()
+                if (abs(position.first - newPosition.first) > 3.0 ||
+                    abs(position.second - newPosition.second) > 3.0){
+
+                    val sx = (newPosition.first - position.first)
+                    val sy = (newPosition.second - position.second)
+                    val dx = sx / speed * 0.03
+                    val dy = sy / speed * 0.03
+                    position = position.first + dx to position.second + dy
+                    updateCoord(
+                        position
+                    )
+                }
+            }
+        }
     }
 
     fun updateCoord(newPosition : Pair<Double,Double>){
